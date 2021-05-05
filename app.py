625b1802aaab641497162a2974ba6003f4d28b3b
Franklin Tan <fjt7@njit.edu>
2021-05-04 21:32:01 -0400

    Feature: added session config

--- app.py
+++ app.py
@@ -9,3 +9,7 @@ load_dotenv()
 
 # App config
 app = Flask(__name__)
+# Session config
+app.secret_key = os.getenv("APP_SECRET_KEY")
+app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
