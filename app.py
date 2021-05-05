45847e364a81b3e00129d77a9a315c7a98a887b8
Franklin Tan <fjt7@njit.edu>
2021-05-04 21:42:34 -0400

    Feature: added oauth register standard register template

--- app.py
+++ app.py
@@ -4,6 +4,17 @@ from authlib.integrations.flask_client import OAuth
 
 app = Flask(__name__)
 oauth = OAuth(app)
+oauth.register(
+    name='google',
+    client_id='',
+    client_secret='',
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    client_kwargs={'scope': 'openid email profile'},
+)
 
 @app.route('/')
 def hello_user():
