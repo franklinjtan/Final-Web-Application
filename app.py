3d497f8e5fb022f3428f90befb738ab42ff17b77
Franklin Tan <fjt7@njit.edu>
2021-05-04 21:38:48 -0400

    Feature: added welcome route

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 # From Flask OAuth Clients
 from flask import Flask, redirect, url_for, session
 from authlib.integrations.flask_client import OAuth
-import os
-from datetime import timedelta
-from auth_decorator import login_required
-from dotenv import load_dotenv
-load_dotenv()
 
-# App config
 app = Flask(__name__)
-# Session config
-app.secret_key = os.getenv("APP_SECRET_KEY")
-app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
+oauth = OAuth(app)
+
+@app.route('/')
+def hello_user():
+    return 'Welcome!'
+
+
+
