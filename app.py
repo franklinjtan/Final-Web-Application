7a4cb704bec4ad19dafe0c114c6389dcc39cf7d7
Franklin Tan <fjt7@njit.edu>
2021-05-04 21:31:12 -0400

    Feature: added app config

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-from flask import Flask, redirect, url_for, session
 # From Flask OAuth Clients
+from flask import Flask, redirect, url_for, session
 from authlib.integrations.flask_client import OAuth
+import os
+from datetime import timedelta
+from auth_decorator import login_required
+from dotenv import load_dotenv
+load_dotenv()
+
+# App config
+app = Flask(__name__)
