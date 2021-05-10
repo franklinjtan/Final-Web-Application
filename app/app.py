41ece10004b5eb4e2006bc1f078273a628a14f06
Franklin Tan <fjt7@njit.edu>
2021-05-10 12:05:28 -0400

    Update: route configurations

--- app/app.py
+++ app/app.py
@@ -15,7 +15,7 @@ app.config['MYSQL_DATABASE_HOST'] = 'db'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_PORT'] = 3306
-app.config['MYSQL_DATABASE_DB'] = 'citiesData'
+app.config['MYSQL_DATABASE_DB'] = 'stockData'
 mysql.init_app(app)
 
 # Authentication -------------------- #
@@ -38,97 +38,97 @@ def index():
     email = dict(session).get('email', None)
     user = {'username': 'Stock Portfolio'}
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tblCitiesImport')
+    cursor.execute('SELECT * FROM stockPortfolioImport')
     result = cursor.fetchall()
     return render_template('index.html', title='Home', user=user, stocks=result)
 
-@app.route('/view/<int:city_id>', methods=['GET'])
-def record_view(city_id):
+@app.route('/view/<int:stock_id>', methods=['GET'])
+def record_view(stock_id):
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tblCitiesImport WHERE id=%s', city_id)
+    cursor.execute('SELECT * FROM stockPortfolioImport WHERE id=%s', stock_id)
     result = cursor.fetchall()
-    return render_template('view.html', title='View Form', city=result[0])
+    return render_template('view.html', title='View Form', stock=result[0])
 
 
-@app.route('/edit/<int:city_id>', methods=['GET'])
-def form_edit_get(city_id):
+@app.route('/edit/<int:stock_id>', methods=['GET'])
+def form_edit_get(stock_id):
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tblCitiesImport WHERE id=%s', city_id)
+    cursor.execute('SELECT * FROM stockPortfolioImport WHERE id=%s', stock_id)
     result = cursor.fetchall()
-    return render_template('edit.html', title='Edit Form', city=result[0])
+    return render_template('edit.html', title='Edit Form', stock=result[0])
 
 
-@app.route('/edit/<int:city_id>', methods=['POST'])
-def form_update_post(city_id):
+@app.route('/edit/<int:stock_id>', methods=['POST'])
+def form_update_post(stock_id):
     cursor = mysql.get_db().cursor()
-    inputData = (request.form.get('fldName'), request.form.get('fldLat'), request.form.get('fldLong'),
-                 request.form.get('fldCountry'), request.form.get('fldAbbreviation'),
-                 request.form.get('fldCapitalStatus'), request.form.get('fldPopulation'), city_id)
-    sql_update_query = """UPDATE tblCitiesImport t SET t.fldName = %s, t.fldLat = %s, t.fldLong = %s, t.fldCountry = 
-    %s, t.fldAbbreviation = %s, t.fldCapitalStatus = %s, t.fldPopulation = %s WHERE t.id = %s """
+    inputData = (request.form.get('Symbol'), request.form.get('Company_Name'), request.form.get('Rating'),
+                 request.form.get('Weight'), request.form.get('Gain_Loss'),
+                 request.form.get('Gain_Loss_1'), request.form.get('Price'), request.form.get('Price_Target'), stock_id)
+    sql_update_query = """UPDATE stockPortfolioImport t SET t.Symbol = %s, t.Company_Name = %s, t.Rating = %s, t.Weight = 
+    %s, t.Gain_Loss = %s, t.Gain_Loss_1 = %s, t.Price = %s, t.Price_Target = %s, WHERE t.id = %s """
     cursor.execute(sql_update_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
-@app.route('/cities/new', methods=['GET'])
+@app.route('/stocks/new', methods=['GET'])
 def form_insert_get():
-    return render_template('new.html', title='New City Form')
+    return render_template('new.html', title='New stock Form')
 
 
-@app.route('/cities/new', methods=['POST'])
+@app.route('/stocks/new', methods=['POST'])
 def form_insert_post():
     cursor = mysql.get_db().cursor()
-    inputData = (request.form.get('fldName'), request.form.get('fldLat'), request.form.get('fldLong'),
-                 request.form.get('fldCountry'), request.form.get('fldAbbreviation'),
-                 request.form.get('fldCapitalStatus'), request.form.get('fldPopulation'))
-    sql_insert_query = """INSERT INTO tblCitiesImport (fldName,fldLat,fldLong,fldCountry,fldAbbreviation,fldCapitalStatus,fldPopulation) VALUES (%s, %s,%s, %s,%s, %s,%s) """
+    inputData = (request.form.get('Symbol'), request.form.get('Company_Name'), request.form.get('Rating'),
+                 request.form.get('Weight'), request.form.get('Gain_Loss'),
+                 request.form.get('Gain_Loss_1'), request.form.get('Price'), request.form.get('Price_Target'))
+    sql_insert_query = """INSERT INTO stockPortfolioImport (Symbol,Company_Name,Rating,Weight,Gain_Loss,Gain_Loss_1,Price, Price_Target) VALUES (%s, %s,%s, %s,%s, %s,%s) """
     cursor.execute(sql_insert_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
-@app.route('/delete/<int:city_id>', methods=['POST'])
-def form_delete_post(city_id):
+@app.route('/delete/<int:stock_id>', methods=['POST'])
+def form_delete_post(stock_id):
     cursor = mysql.get_db().cursor()
-    sql_delete_query = """DELETE FROM tblCitiesImport WHERE id = %s """
-    cursor.execute(sql_delete_query, city_id)
+    sql_delete_query = """DELETE FROM stockPortfolioImport WHERE id = %s """
+    cursor.execute(sql_delete_query, stock_id)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
 
-@app.route('/api/v1/cities', methods=['GET'])
+@app.route('/api/v1/stocks', methods=['GET'])
 def api_browse() -> str:
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tblCitiesImport')
+    cursor.execute('SELECT * FROM stockPortfolioImport')
     result = cursor.fetchall()
     json_result = json.dumps(result);
     resp = Response(json_result, status=200, mimetype='application/json')
     return resp
 
 
-@app.route('/api/v1/cities/<int:city_id>', methods=['GET'])
-def api_retrieve(city_id) -> str:
+@app.route('/api/v1/stocks/<int:stock_id>', methods=['GET'])
+def api_retrieve(stock_id) -> str:
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tblCitiesImport WHERE id=%s', city_id)
+    cursor.execute('SELECT * FROM stockPortfolioImport WHERE id=%s', stock_id)
     result = cursor.fetchall()
     json_result = json.dumps(result);
     resp = Response(json_result, status=200, mimetype='application/json')
     return resp
 
 
-@app.route('/api/v1/cities/', methods=['POST'])
+@app.route('/api/v1/stocks/', methods=['POST'])
 def api_add() -> str:
     resp = Response(status=201, mimetype='application/json')
     return resp
 
 
-@app.route('/api/v1/cities/<int:city_id>', methods=['PUT'])
-def api_edit(city_id) -> str:
+@app.route('/api/v1/stocks/<int:stock_id>', methods=['PUT'])
+def api_edit(stock_id) -> str:
     resp = Response(status=201, mimetype='application/json')
     return resp
 
 
-@app.route('/api/cities/<int:city_id>', methods=['DELETE'])
-def api_delete(city_id) -> str:
+@app.route('/api/stocks/<int:stock_id>', methods=['DELETE'])
+def api_delete(stock_id) -> str:
     resp = Response(status=210, mimetype='application/json')
     return resp
 
